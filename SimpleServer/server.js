85d1193ead353487f66cb8770258c6dfe824aa5f
tjohnson7021 <86614374+tjohnson7021@users.noreply.github.com>
2021-07-31 18:37:46 -0400

    Completed Basic Website with Node.js server

--- SimpleServer/server.js
+++ SimpleServer/server.js
@@ -1,50 +1,47 @@
-/*This is not usualy a file someone would build - there are now frameworks like Express that
-generate this type of code for us */
-
 const http = require('http');
 const url = require('url');
 const path = require('path');
 const fs = require('fs');
 
 const mimeTypes = {
-    "html": "text/html",
-    "jpeg": "image/jpeg",
-    "jpg": "image/jpg",
-    "png": "image/png",
-    "js": "text/javascript",
-    "css": "text/css"
+	"html": "text/html",
+	"jpeg": "image/jpeg",
+	"jpg": "image/jpg",
+	"png": "image/png",
+	"js": "text/javascript",
+	"css": "text/css"
 };
 
 http.createServer(function(req, res){
-    var uri = url.parse(req.url).pathname;
-    var fileName = path.join(process.cwd(), unescape(uri));
-    console.log('Loading ' + uri);
-    var stats;
+	var uri = url.parse(req.url).pathname;
+	var fileName = path.join(process.cwd(), unescape(uri));
+	console.log('Loading '+ uri);
+	var stats;
 
-    try{
-        stats = fs.lstatSync(fileName);
-    } catch(e){
-        res.writeHead(404, {'Content-type' : 'text/plain'});
-        res.write('404 Not Found\n');
-        res.end();
-        return;
-    }
+	try{
+		stats = fs.lstatSync(fileName);
+	} catch(e){
+		res.writeHead(404, {'Content-type': 'text/plain'});
+		res.write('404 Not Found\n');
+		res.end();
+		return;
+	}
 
-    if(stats.isFile()){ // checks if url is a file to be served
-        var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
-        res.writeHead(200, {'Content-type' : mimeType});
+	if(stats.isFile()){
+		var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
+		res.writeHead(200, {'Content-type': mimeType});
 
-        var fileStream = fs.createReadStream(fileName);
-        fileStream.pipe(res);
-    } else if (stats.isDirectory) { // checks if url is a directory
-        res.writeHead(302,{
-            'Location' : 'index.html'
-        });
-        res.end();
-    } else {
-        res.writeHead(500, {'Content-type' : 'text/plain'});
-        res.write('500 Internal Error\n');
-        res.end();
-    }
+		var fileStream = fs.createReadStream(fileName);
+		fileStream.pipe(res);
+	} else if(stats.isDirectory()) {
+		res.writeHead(302, {
+			'Location': 'index.html'
+		});
+		res.end();
+	} else {
+		res.writeHead(500, {'Content-type':'text/plain'});
+		res.write('500 Internal Error\n');
+		res.end();
+	}
 
-}).listen(3000);
+}).listen(1337);
